3f2fd961fc04706e3ae5e43d1a3042a780d5f9f5
maddyCoder <103992158+maddyyyyyyy6@users.noreply.github.com>
2023-04-16 17:57:02 +0530

    ADDED option for file input with urls

--- bulk.py
+++ bulk.py
@@ -1,10 +1,15 @@
 from pytube import YouTube
-
+# use a file with urls
+# or a other .py with array of urls to keep the code neat
 # Replace this list with the YouTube video URLs you want to download
 video_urls = ['url1', 'url2']
-# we can also do this from a file with url names
 
-for url in video_urls:
+# we can also do this from a file with url names
+with open("urls.txt", "r") as file:
+    # Read the contents of the file and split by new line character
+    urls = file.read().split("\n")
+
+for url in urls:
     try:
         video = YouTube(url)
         print(f"Downloading '{video.title}'...")
